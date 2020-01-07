e23811ee0408669acde1bb3ad6eb6ef0f9a40a5a
suryafakhriy <55834014+suryafakhriy@users.noreply.github.com>
2020-01-08 00:12:03 +0700

    Add files via upload

---
+++ LatihanMVC/src/edu/suryafakhriyhasbi/latihanmvc/controller/PelangganContoller.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.suryafakhriyhasbi.latihanmvc.controller;
+
+import edu.suryafakhriyhasbi.latihanmvc.model.PelangganModel;
+import edu.suryafakhriyhasbi.latihanmvc.view.PelangganView;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Azis Komara
+ */
+public class PelangganContoller {
+    
+    private PelangganModel model;
+
+    public void setModel(PelangganModel model) {
+        this.model = model;
+    }
+    
+    
+    public void resetForm(PelangganView view){
+        
+        String nama = view.getTxtNama().getText();
+        String email = view.getTxtEmail().getText();
+        String noTlp = view.getTxtTelp().getText();
+        
+        if (nama.equals("") && email.equals("") && noTlp.equals("")){
+            
+        } else{
+            model.resetForm();
+        }
+    }
+    
+    public void simpanForm(PelangganView view){
+        String nama = view.getTxtNama().getText();
+        String email = view.getTxtEmail().getText();
+        String noTlp = view.getTxtTelp().getText();
+        
+        if (nama.trim().equals("")) {
+                JOptionPane.showMessageDialog(view, "Nama Masih Kosong");
+            } else if (email.trim().equals("")){
+                JOptionPane.showMessageDialog(view, "Email Masih Kosong");
+            }else if (noTlp.trim().equals("")){
+                JOptionPane.showMessageDialog(view, "No Telepon Masih Kosong");
+            }else{
+                model.simpanForm();
+            }
+    }
+}
---
+++ LatihanMVC/src/edu/suryafakhriyhasbi/latihanmvc/event/PelangganListener.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.suryafakhriyhasbi.latihanmvc.event;
+
+import edu.suryafakhriyhasbi.latihanmvc.model.PelangganModel;
+
+/**
+ *
+ * @author Azis Komara
+ */
+public interface PelangganListener {
+    
+    public void onChange(PelangganModel pelanggan);
+}
